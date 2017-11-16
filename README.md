94dd146191c9f1292f6bc7f100d82cea8040c636
Doino Gretchenliev <doino.gretchenliev@ontotext.com>
2017-11-16 19:28:19 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Description
+-----------
+The [setup-prepare-commit-msg](setup-prepare-commit-msg.py) scans user workspace directory and configures every local git repository with prepare-commit-msg hook.
+The hook is configured with [prepare-commit-msg.sh](https://gist.github.com/bartoszmajsak/1396344#file-prepare-commit-msg-sh) script.
+
+Requirments
+-----------
+Python 2 or up
+
+Installtion
+-----------
+* As is
+    No installation required
+* As cron job
+    1. Enter cron editing mode: 
+    `crontab -e`
+    2. Add the following line:
+    `0 8 * * * <path_to_setup-prepare-commit-msg.py>`
+
+Usage
+-----
+Check `setup-prepare-commit-msg.py --help`
---
+++ setup-prepare-commit-msg.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+import fnmatch
+import os
+import stat
+
+parser = argparse.ArgumentParser(
+    description='Setup git precommit message formatting')
+parser.add_argument('-w', '--workspace-path', metavar='<path-to-workspace-dir>',
+                    help='Path to your workspace directory', required=True,
+                    dest="workspace_path")
+args = parser.parse_args()
+
+workspace_path = args.workspace_path
+
+prepare_commit_msg_file_content = """#!/bin/bash
+
+# This way you can customize which branches should be skipped when
+# prepending commit message. 
+if [ -z "$BRANCHES_TO_SKIP" ]; then
+  BRANCHES_TO_SKIP=(master)
+fi
+
+BRANCH_NAME=$(git symbolic-ref --short HEAD)
+BRANCH_NAME="${BRANCH_NAME##*/}"
+
+BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
+
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
+  sed -i.bak -e "1s/^/[$BRANCH_NAME] /" $1
+fi
+"""
+
+print "Start searching for local git repositories at [{}]".format(
+    workspace_path)
+git_repositories_count = 0
+for root, directory_names, file_names in os.walk(workspace_path):
+  for git_directory_name in fnmatch.filter(directory_names, '*.git'):
+    git_directory_path = os.path.join(root, git_directory_name)
+    prepare_commit_msg_file_path = os.path.join(git_directory_path,
+                                                'hooks/prepare-commit-msg')
+
+    print "Git repository found at [{}], writing file [{}]".format(
+        git_directory_path, prepare_commit_msg_file_path)
+    prepare_commit_msg_file = open(prepare_commit_msg_file_path, 'w')
+    prepare_commit_msg_file.write(prepare_commit_msg_file_content)
+
+    print "Fix file permissions [{}]".format(prepare_commit_msg_file_path)
+    prepare_commit_msg_file_stat = os.stat(prepare_commit_msg_file_path)
+    os.chmod(prepare_commit_msg_file_path,
+             prepare_commit_msg_file_stat.st_mode | stat.S_IEXEC)
+    print "Processing file [{}] completed successfully".format(
+        prepare_commit_msg_file_path)
+    git_repositories_count += 1
+
+print "Git repositories found and processed [{}]".format(git_repositories_count)
